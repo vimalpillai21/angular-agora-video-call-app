7bb28ba7f3ab832855840482e3b69c197a4b0e7d
Vimal Pillai <vimalpillai@goodbits.in>
2019-05-27 16:12:13 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularAgoraRtcService, Stream } from 'angular-agora-rtc'; 
 
 @Component({
@@ -6,13 +6,15 @@ import { AngularAgoraRtcService, Stream } from 'angular-agora-rtc';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angagora';
   localStream: Stream;
+  remoteCalls:any = [];
   
   constructor( private agoraService: AngularAgoraRtcService){
     this.agoraService.createClient();
   }
+  
 
     // Add
     startCall() {
@@ -44,6 +46,51 @@ export class AppComponent {
         });
       }, function (err) {
         console.log("getUserMedia failed", err);
+      });
+  
+      // Add
+      this.agoraService.client.on('error', (err) => {
+        console.log("Got error msg:", err.reason);
+        if (err.reason === 'DYNAMIC_KEY_TIMEOUT') {
+          this.agoraService.client.renewChannelKey("", () => {
+            console.log("Renew channel key successfully");
+          }, (err) => {
+            console.log("Renew channel key failed: ", err);
+          });
+        }
+      });
+  
+      // Add
+      this.agoraService.client.on('stream-added', (evt) => {
+        const stream = evt.stream;
+        this.agoraService.client.subscribe(stream, (err) => {
+          console.log("Subscribe stream failed", err);
+        });
+      });
+  
+      // Add
+      this.agoraService.client.on('stream-subscribed', (evt) => {
+        const stream = evt.stream;
+        if (!this.remoteCalls.includes(`agora_remote${stream.getId()}`)) this.remoteCalls.push(`agora_remote${stream.getId()}`);
+        setTimeout(() => stream.play(`agora_remote${stream.getId()}`), 2000);
+      });
+  
+      // Add
+      this.agoraService.client.on('stream-removed', (evt) => {
+        const stream = evt.stream;
+        stream.stop();
+        this.remoteCalls = this.remoteCalls.filter(call => call !== `#agora_remote${stream.getId()}`);
+        console.log(`Remote stream is removed ${stream.getId()}`);
+      });
+  
+      // Add
+      this.agoraService.client.on('peer-leave', (evt) => {
+        const stream = evt.stream;
+        if (stream) {
+          stream.stop();
+          this.remoteCalls = this.remoteCalls.filter(call => call === `#agora_remote${stream.getId()}`);
+          console.log(`${evt.uid} left from this channel`);
+        }
       });
     }
 }
